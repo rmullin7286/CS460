39a5d4a774b3e938d229ca013a3ccb8aa5ad5536
Ryan Mullin <ryan.mullin@wsu.edu>
2019-04-24 11:30:17 -0700

    Downloaded initial files for final project

---
+++ LAST/r
@@ -0,0 +1,2 @@
+qemu-system-arm -M versatilepb -m 128M -sd sdimage -kernel wanix \
+-serial mon:stdio -serial /dev/pts/1
---
+++ LAST/test.c
@@ -0,0 +1,16 @@
+/********** test.c file *************/
+#include "ucode.c"
+
+int main(int argc, char *argv[ ])
+{
+  int i;
+
+  int pid = getpid();
+  printf("KCW: PROC %d running test program\n", pid);
+
+  printf("argc = %d\n", argc);
+  for (i=0; i<argc; i++)
+    printf("argv[%d] = %s\n", i, argv[i]);
+
+  printf("PROC %d exit\n", pid);
+}
